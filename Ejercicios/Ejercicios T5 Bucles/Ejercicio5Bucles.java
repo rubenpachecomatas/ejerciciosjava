f0a3dc23545bfc0d77ff4bc6399994a351aaa815
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2018-10-10 20:29:48 +0200

    Add files via upload

---
+++ Ejercicios/Ejercicios T5 Bucles/Ejercicio5Bucles.java
@@ -0,0 +1,22 @@
+/*
+ * Muestra los números del 320 al 160 contando hacia atrás de 20 en 20 con un
+ * bucle While.
+ * 
+ * @author Rubén Pacheco Matas
+ */
+
+
+public class Ejercicio5Bucles {
+	
+	public static void main (String[] args) {
+		
+    int x = 340;
+    
+    while (x > 160) {
+      x = x - 20;
+      System.out.println(x + " ");
+    }
+    
+	}
+}
+
