fb43490cd94aafa913169df57ac473ae8e1cda5a
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2018-10-10 20:20:51 +0200

    Add files via upload

---
+++ Ejercicios/Ejercicios T5 Bucles/Ejercicio3Bucles.java
@@ -0,0 +1,21 @@
+/*
+ * Muestra los números múltiplos de 5 entre 0 y 100 con un bucle Do-While.
+ * 
+ * @author Rubén Pacheco Matas
+ */
+
+
+public class Ejercicio3Bucles {
+	
+	public static void main (String[] args) {
+		
+    int x = 0;
+    do {
+      x = x + 5;
+      System.out.println(x + " ");
+    }
+    while (x < 100);
+    
+	}
+}
+
