950bbda4ea6e921311d6147b794fb1db3d11ae27
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2018-10-10 20:18:28 +0200

    Add files via upload

---
+++ Ejercicios/Ejercicios T5 Bucles/Ejercicio1Bucles.java
@@ -0,0 +1,18 @@
+/*
+ * Muestra los números múltiplos de 5 entre 0 y 100 con un bucle FOR.
+ * 
+ * @author Rubén Pacheco Matas
+ */
+
+
+public class Ejercicio1Bucles {
+	
+	public static void main (String[] args) {
+		
+    for (int x = 5; x < 100; x = x + 5) {
+      System.out.println(x + " ");
+    }
+    
+	}
+}
+
