3af4658278b4224ecbe9ca9f3e66bd34f8198927
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2018-11-27 13:35:42 +0100

    Add files via upload

---
+++ Ejercicio15Funciones.java
@@ -0,0 +1,17 @@
+
+package tema.pkg8.funciones;
+
+/**
+ *
+ * @author ODLana
+ */
+public class Ejercicio15Funciones {
+    
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            if (matematicas.funciones.esPrimo(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
