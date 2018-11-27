a4e5a68f74ff2a785b7f37492dff68a5ae2d25bc
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2018-11-27 13:43:49 +0100

    Add files via upload

---
+++ Ejercicios/Ejercicios T8 Funciones/Ejercicio16Funciones.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tema.pkg8.funciones;
+
+/**
+ *
+ * @author ODLana
+ */
+public class Ejercicio16Funciones {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 99999; i++) {
+            if (matematicas.funciones.esCapicua(i)) {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
