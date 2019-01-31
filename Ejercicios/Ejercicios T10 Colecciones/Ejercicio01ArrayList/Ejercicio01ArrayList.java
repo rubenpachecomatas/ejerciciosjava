c23bae1a7d047de6457d7c10672f19b69cd53d86
Rubén Pacheco Matas <43571185+rubenpachecomatas@users.noreply.github.com>
2019-01-31 09:24:14 +0100

    Add files via upload

---
+++ Ejercicios/Ejercicios T10 Colecciones/Ejercicio01ArrayList/Ejercicio01ArrayList.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio01ArrayList;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ODLana
+ */
+public class Ejercicio01ArrayList {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> n = new ArrayList<String>();
+        
+        n.add("Compa1");
+        n.add("Compa2");
+        n.add("Compa3");
+        n.add("Compa4");
+        n.add("Compa5");
+        
+        /*for (int i = 0; i < n.size(); i++) {
+            System.out.println(n.get(i));
+        */
+        
+        System.out.println(n);
+        
+    }
+    
+}
